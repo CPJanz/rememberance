50ca00783943b5a878c3bd694ede4149fd2d996b
Carl Janz <carl.janz@gmail.com>
2019-04-26 23:07:31 -0700

    Updates

---
+++ precache-manifest.84e435d1b293de7ab8ac1fb630c4a2f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7473d4c82f7b4b5f9b45bf602446a02a",
+    "url": "/rememberance/index.html"
+  },
+  {
+    "revision": "84737e316368c0714e80",
+    "url": "/rememberance/static/css/main.424df928.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rememberance/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "84737e316368c0714e80",
+    "url": "/rememberance/static/js/main.3ab4be35.chunk.js"
+  },
+  {
+    "revision": "724c404dad134fb5411a",
+    "url": "/rememberance/static/js/runtime~main.0cb4884c.js"
+  }
+]);
